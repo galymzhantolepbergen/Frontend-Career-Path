74e7b92d54e107876109275243614e5be12ed874
Galymzhan <galymzhan.tolepbergen@gmail.com>
2024-02-13 23:59:20 +0600

    description changed

--- Web dev basics/Building a Personal Website/index.js
+++ Web dev basics/Building a Personal Website/index.js
@@ -1,23 +1,13 @@
-// Challenge 2:
-// Replace the arguments below according to your preference.
 
-// space, scary, military, romantic, cowboy, fantasy, superhero
 favouriteMovieGenre("fantasy")
 
-// watermelon, tomato, banana, orange, avocado, blueberry
 favouriteFruit("banana")
 
-// light, dark
 favouriteMode("dark")
 
-// sharp, soft, round
 favouriteEdgeStyle("soft")
 
 
-
-////////////////////////////////////
-// IGONE THE CODE BELOW THIS LINE //
-////////////////////////////////////
 
 function setProp(prop, value) {
     document.documentElement.style.setProperty(prop, value)
